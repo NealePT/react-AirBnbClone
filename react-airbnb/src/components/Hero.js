4c4fd7cf665c023aa0102aacd6be87d1a17fbe38
Neale Taylor <nealeptaylor@gmail.com>
2022-06-27 13:28:01 -0400

    add content to hero

---
+++ react-airbnb/src/components/Hero.js
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <section className="Hero">
+      <img src="../images/airbnbGroupGrid.png" alt="Collection of Photos"></img>
+      <h1>Online Experiences</h1>
+      <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </section> 
+  )
+}
