c7649f9700ca2f2c113c3d5d035bfb010f5b744a
Neale Taylor <nealeptaylor@gmail.com>
2022-06-27 13:29:02 -0400

    add classnames

--- react-airbnb/src/components/Hero.js
+++ react-airbnb/src/components/Hero.js
@@ -1,9 +1,9 @@
 export default function Hero() {
   return (
     <section className="Hero">
-      <img src="../images/airbnbGroupGrid.png" alt="Collection of Photos"></img>
-      <h1>Online Experiences</h1>
-      <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      <img className="heroPhoto" src="../images/airbnbGroupGrid.png" alt="Collection of Photos"></img>
+      <h1 className="heroHeader">Online Experiences</h1>
+      <p className="heroPara">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
     </section> 
   )
 }
